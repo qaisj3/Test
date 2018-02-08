e5852066dac5986b32ba406d45b5f7d802c15316
qaisj3 <qaisj3@gmail.com>
2018-02-08 14:30:16 +0800

    Create server.c

---
+++ server.c
@@ -0,0 +1,44 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr;
+
+    char sendBuff[6];
+    char sendBuffx[6] = {0x68, 0x04, 0x07, 0x01, 0x01, 0x01};
+
+    time_t ticks;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(2404);
+
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    listen(listenfd, 10);
+    while(1){
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+
+        ticks = time(NULL);
+        memcpy(sendBuff, sendBuffx, sizeof(sendBuff));
+        write(connfd, sendBuff, strlen(sendBuff));
+
+        close(connfd);
+        sleep(1);
+     }
+
+}
